046159e01ab831b0ed3e2d1c998a4a8ac8055a05
kyle15451 <kyle.kitchener@outlook.com>
2025-06-26 05:59:50 +0200

    added basic storage state

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TodoFormComponnent } from './ui/todo-form.component';
 import { Todo } from '../shared/interfaces/todo';
+import { TodoService } from '../shared/interfaces/data=access/todo.service';
+import { TodoListComponent } from "./ui/todo-list.component";
 
 @Component({
   selector: 'app-home',
   template: `
   <h2>This is the home component</h2>
-  <app-todo-form (todoSubmitted)="createTodo($event)"/>
+  <app-todo-list [todos]="todoService.todos()"/>
+  <app-todo-form (todoSubmitted)="todoService.addTodo($event)"/>
   `,
-  imports: [TodoFormComponnent],
+  imports: [TodoFormComponnent, TodoListComponent,TodoListComponent],
 })
 export default class HomeComponent {
-  createTodo(todo: Todo){
-    console.log(todo);
-  }
+
+  todoService = inject(TodoService);
+ 
 }
